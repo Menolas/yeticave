594fd7dd259897b1b5be9f84f7af29d87bea1aef
Olena Kunina <elena@Olenas-MacBook-Pro.local>
2018-12-02 08:41:53 +0300

    connect

--- index.php
+++ index.php
@@ -7,40 +7,12 @@ date_default_timezone_set ('Europe/Moscow');
 
 require('functions.php');
 
+$link = connect_db ();
 
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
-$lots = [
-  [
-    'name' => '2014 Rossignol District Snowboard',
-    'category' => 'Доски и лыжи',
-    'price' => '10999',
-    'url_path' => 'img/lot-1.jpg'],
-  [
-    'name' => 'DC Ply Mens 2016/2017 Snowboard',
-    'category' => 'Доски и лыжи',
-    'price' => '159999',
-    'url_path' => 'img/lot-2.jpg'],
-  [
-    'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-    'category' => 'Крепления',
-    'price' => '8000',
-    'url_path' => 'img/lot-3.jpg'],
-  [
-    'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-    'category' => 'Ботинки',
-    'price' => '10999',
-    'url_path' => 'img/lot-4.jpg'],
-  [
-    'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-    'category' => 'Одежда',
-    'price' => '7500',
-    'url_path' => 'img/lot-5.jpg'],
-  [
-    'name' => 'Маска Oakley Canopy',
-    'category' => 'Разное',
-    'price' => '5400',
-    'url_path' => 'img/lot-6.jpg']
-];
+
+$categories = get_categories ($link);
+$lots = get_lots($link);
+
 $is_auth = rand(0, 1);
 
 $user_name = 'Menolas'; // укажите здесь ваше имя
--- templates/index.php
+++ templates/index.php
@@ -6,7 +6,7 @@
         <ul class="promo__list">
             <?php foreach ($categories as $category): ?>
                 <li class="promo__item promo__item--boards">
-                    <a class="promo__link" href="pages/all-lots.html"><?=$category;?></a>
+                    <a class="promo__link" href="pages/all-lots.html"><?=$category['name'];?></a>
                 </li>
             <?php endforeach; ?>
             
@@ -20,15 +20,15 @@
             <?php foreach ($lots as $lot): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?=$lot['url_path'];?>" width="350" height="260" alt="">
+                        <img src="<?=$lot['image'];?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$lot['category'];?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=esc($lot['name']);?></a></h3>
+                        <span class="lot__category"><?=$lot['category_name'];?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=esc($lot['title']);?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
-                                <span class="lot__amount"><?=$lot['price'];?></span>
-                                <span class="lot__cost"><?=format_sum($lot['price']);?></span>
+                                <span class="lot__amount"><?=$lot['start_price'];?></span>
+                                <span class="lot__cost"><?=format_sum($lot['start_price']);?></span>
                             </div>
                             <div class="lot__timer timer">
                                 <?=time_left_till_midnight();?>
